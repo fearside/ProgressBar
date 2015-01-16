94e18ad3d8143615de176cb2fed342c94388d977
Teddy Skarin <tsn@bythjul.com>
2015-01-16 09:29:15 +0100

    Cleanup.

--- progressbar.sh
+++ progressbar.sh
@@ -15,7 +15,7 @@ function ProgressBar {
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:
 # 1.2.1.1 Progress : [########################################] 100%
-printf '\rProgress : ['${_fill// /#}${_empty// /-}'] '${_progress}'%%'
+printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
 
 }
 
