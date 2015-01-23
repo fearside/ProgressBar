5087d5ada1eefecaec2d4d10522e1d30e8b80d55
Teddy Skarin <tsn@bythjul.com>
2015-01-23 15:26:16 +0100

    Cleanup of code.

--- progressbar.sh
+++ progressbar.sh
@@ -9,13 +9,13 @@ function ProgressBar {
 	let _done=(${_progress}*4)/10
 	let _left=40-$_done
 # Build progressbar string lengths
-	_fill=$(printf "%${_done}s")
-	_empty=$(printf "%${_left}s")
+	_done=$(printf "%${_done}s")
+	_left=$(printf "%${_left}s")
 
 # 1.2 Build progressbar strings and print the ProgressBar line
 # 1.2.1 Output example:
 # 1.2.1.1 Progress : [########################################] 100%
-printf "\rProgress : [${_fill// /#}${_empty// /-}] ${_progress}%%"
+printf "\rProgress : [${_done// /#}${_left// /-}] ${_progress}%%"
 
 }
 
